18e22067e493979fdcc670e160f1b0ccfd4e8c6b
sergiioss <sergiiptx@gmail.com>
2022-11-04 12:27:21 +0100

    Endpoint get de Bienvenida

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,9 @@ const express = require('express');
 const api = express.Router();
 const auth = require('../middlewares/auth');
 
+api.get('/', (req, res) =>{
+    res.status(200).send('Bienvenido al Taller!!')
+})
+
 
 module.exports = api;
