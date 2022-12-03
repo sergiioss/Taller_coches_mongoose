40deb8a1865869fb50cac203fa82754ec7fccaab
sergiioss <sergiiptx@gmail.com>
2022-12-03 15:41:51 +0100

    Fixed 7 change config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port:process.env.PORT || 3000,
-    db:process.env.MONGODB || 'mongodb://localhost:27017/taller-coches',
+    db:process.env.MONGODB || 'mongodb+srv://taller:123@taller.xccwcj8.mongodb.net/?retryWrites=true&w=majority',
     SECRET_TOKEN: 'miclavedetokens'
 }
