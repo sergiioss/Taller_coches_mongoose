e052d26aa66bfd2a11531189a732612e7141e634
sergiioss <sergiiptx@gmail.com>
2022-12-03 15:54:09 +0100

    Fixed 10 change config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port:process.env.PORT || 3000,
-    db:process.env.MONGODB || `mongodb+srv://taller:123@taller.xccwcj8.mongodb.net/?retryWrites=true&w=majority`,
+    db:process.env.MONGODB || "mongodb+srv://taller:123@taller.xccwcj8.mongodb.net/?retryWrites=true&w=majority",
     SECRET_TOKEN: 'miclavedetokens'
 }
